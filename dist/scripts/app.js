dbfde1d15998423663ec4873d95e5fcf933b03b5
Darcey Iwashita <darcey@gmail.com>
2017-03-04 21:00:12 -1000

    <nav> content added to index.html

---
+++ dist/scripts/app.js
@@ -0,0 +1,5 @@
+angular.module('blocJams', []);
+//The first argument passed, blocJams, is the prescribed name of the module. 
+//empty array, passed as the second argument, injects dependencies into an application
+
+
