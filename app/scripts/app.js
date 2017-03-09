af3a77d1e402d6f4a7589789bb1f97017259a468
Darcey Iwashita <darcey@gmail.com>
2017-03-08 15:52:38 -1000

    ui-sref replaced href; collection template added

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -25,6 +25,10 @@
         	.state('album', {
         		url: '/album',
         		templateUrl: '/templates/album.html'
+        	})
+        	.state('collection', {
+        		url: '/collection',
+        		templateUrl: '/templates/collection.html'
         	});
         	//chained calls
 	}
