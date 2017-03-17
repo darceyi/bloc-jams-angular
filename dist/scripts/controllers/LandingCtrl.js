3298cc20f1006e0ab90acf9d2c6619921f298635
Darcey Iwashita <darcey@gmail.com>
2017-03-17 10:02:59 -1000

    landing controller givenlogic added

--- dist/scripts/controllers/LandingCtrl.js
+++ dist/scripts/controllers/LandingCtrl.js
@@ -1,7 +1,7 @@
 (function() {
 	function LandingCtrl() {
-
-	}
+		//Using the this keyword adds heroTitle as a property on the LandingCtrl's $scope object.
+		this.heroTitle = "Turn the Music Up!";
 
 	angular
 		//Note that the .module() call does not have the second argument, the array of dependencies. Because we've set the 
@@ -10,5 +10,7 @@
 		//The .controller() method has two parameters:
 		//Name
 		//A callback function (OR an array that injects dependencies, with a callback function as the last item in the array).
+
+		//The LandingCtrl for Bloc Jams is registered for the landing.html template.
 		.controller('LandingCtrl', LandingCtrl);
 })();
