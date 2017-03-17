52d589b5a04f654d2261d2e23e14ade1340c46c0
Darcey Iwashita <darcey@gmail.com>
2017-03-17 13:43:55 -1000

    controller for landing fixed

--- app/scripts/controllers/LandingCtrl.js
+++ app/scripts/controllers/LandingCtrl.js
@@ -2,6 +2,7 @@
 	function LandingCtrl() {
 		//Using the this keyword adds heroTitle as a property on the LandingCtrl's $scope object.
 		this.heroTitle = "Turn the Music Up!";
+	}
 
 	angular
 		//Note that the .module() call does not have the second argument, the array of dependencies. Because we've set the 
@@ -13,7 +14,4 @@
 
 		//The LandingCtrl for Bloc Jams is registered for the landing.html template.
 		.controller('LandingCtrl', LandingCtrl);
-	}
 })();
-
-//
