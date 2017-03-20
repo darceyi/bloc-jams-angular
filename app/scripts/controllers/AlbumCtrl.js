6ad4937c1ea8fd7115441b2ac65142905b756f5c
Darcey Iwashita <darcey@gmail.com>
2017-03-19 17:48:57 -1000

    Fixtures service added to AlbumCtrl

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,9 +1,12 @@
 (function() {
 	function AlbumCtrl() {
-		this.albumData = angular.copy(albumPicasso);
+		this.albumData = Fixtures.getAlbum();
 	}
 	
 	angular
 		.module('blocJams')
-		.controller('AlbumCtrl', AlbumCtrl);
+		.controller('AlbumCtrl', AlbumCtrl)
+		.controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
+		//We add Fixtures to AlbumCtrl's array of dependencies. Once injected, 
+		//the service is available for use within the controller.
 })();
