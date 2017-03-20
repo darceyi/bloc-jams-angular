656cacc2d5331a91f1d9a5e42be032c4d8cedece
Darcey Iwashita <darcey@gmail.com>
2017-03-19 20:18:59 -1000

    SongPlayer service created

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,12 +1,13 @@
 (function() {
-	function AlbumCtrl(Fixtures) {
+	function AlbumCtrl(Fixtures, SongPlayer) {
 		this.albumData = Fixtures.getAlbum();
+		this.songPlayer = SongPlayer;
 		//AlbumCtrl uses Fixtures's getAlbum() method to get the albumPicasso object.
 	}
 	
 	angular
 		.module('blocJams')
-		.controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
+		.controller('AlbumCtrl', ['Fixtures', 'SongPlayer', AlbumCtrl]);
 		//We add Fixtures to AlbumCtrl's array of dependencies. Once injected, 
 		//the service is available for use within the controller.
 })();
