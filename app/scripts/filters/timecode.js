f7cf70445a6b6b524d0cc8689e916839b6b4046e
Darcey Iwashita <darcey@gmail.com>
2017-03-27 09:50:39 -1000

    buzz.toTimer() function replaced previous timecode function

--- app/scripts/filters/timecode.js
+++ app/scripts/filters/timecode.js
@@ -3,25 +3,13 @@
 	function timecode() {
 		return function(seconds) {
 
-			var seconds = Number.parseFloat(seconds);
+			var toSeconds = buzz.toTimer(seconds);
 
 			if (Number.isNaN(seconds)) {
-				return '-:--';
+				return '';
 			}
 
-            var wholeSeconds = Math.floor(seconds);
-            var minutes = Math.floor(wholeSeconds / 60);
-            var remainingSeconds = wholeSeconds % 60;
- 
-            var output = minutes + ':';
- 
-            if (remainingSeconds < 10) {
-                output += '0';   
-            }
- 
-            output += remainingSeconds;
-
-			return output;
+				return toSeconds;
 		};
 	}
 
